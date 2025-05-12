35a4c158ffc771a0fc515ff8240fc3afdc4bcc73
BUSHBOI95 <jphilbincrafts@gmail.com>
2025-05-12 16:35:18 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import './index.css';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-document.querySelector('#root').innerHTML = App();
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
