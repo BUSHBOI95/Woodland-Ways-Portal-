f412c9789e11c0a8fa51ed33b65ec379da47da9e
BUSHBOI95 <jphilbincrafts@gmail.com>
2025-05-13 20:57:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
+    outDir: 'dist'
   }
 });
