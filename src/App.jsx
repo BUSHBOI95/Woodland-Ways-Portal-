3e3b2b99f7119fe4ff25ff95d3fe8139c3202bf5
BUSHBOI95 <jphilbincrafts@gmail.com>
2025-05-13 13:56:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
- import React from 'react';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
+import MyCourses from './pages/MyCourses';
+import Calendar from './pages/Calendar';
+import Handbook from './pages/Handbook';
+import MenuPage from './pages/Menu';
+import Photos from './pages/Photos';
+import Events from './pages/Events';
+import Directory from './pages/Directory';
+import Login from './pages/Login';
+import SplashScreen from './pages/SplashScreen';
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/my-courses" element={<MyCourses />} />
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="/handbook" element={<Handbook />} />
+      <Route path="/menu" element={<MenuPage />} />
+      <Route path="/photos" element={<Photos />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/directory" element={<Directory />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/splash" element={<SplashScreen />} />
+    </Routes>
+  );
 };
 
 export default App;
