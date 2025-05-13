1597cbfbc2fa4b2d873f743d76c2e538b8144713
BUSHBOI95 <jphilbincrafts@gmail.com>
2025-05-13 14:04:35 +0100

    Update Directory.jsx

--- src/pages/Directory.jsx
+++ src/pages/Directory.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const Directory = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <h1 className="text-xl font-bold text-gray-800">Directory Page</h1>
+    </div>
+  );
+};
+
+export default Directory;
